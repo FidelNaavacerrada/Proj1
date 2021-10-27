1bdeabbef96afee8cfa559952fe4e2ff9ceb5d11
FidelNaavacerrada <fideliofifa@gmail.com>
2021-10-26 21:06:20 -0400

    push puh baby

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <button onClick={bruh}> butt</button>
+        <button className="button-test" onClick={bruh}> butt</button>
         <a
           className="App-link"
           href="https://reactjs.org"
